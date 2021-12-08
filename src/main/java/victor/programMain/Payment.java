375306386f80ad1306def7fcfd2189953d73c6ae
Ion <vaniniabivol@gmail.com>
2021-12-08 16:01:07 +0200

    Payment

--- src/main/java/victor/programMain/Payment.java
+++ src/main/java/victor/programMain/Payment.java
@@ -1,2 +1,69 @@
-package victor.programMain;public class Payment {
+package victor.programMain;
+
+public class Payment {
+
+    private int Id;
+    private int Amount;
+    private String Currency;
+    private boolean IsSuccess;
+    private boolean IsCancel;
+
+    protected Payment(int id, int amount, String currency, boolean isSuccess, boolean isCancel) {
+        Id = id;
+        Amount = amount;
+        Currency = currency;
+        IsSuccess = isSuccess;
+        IsCancel = isCancel;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public int getAmount() {
+        return Amount;
+    }
+
+    public void setAmount(int amount) {
+        Amount = amount;
+    }
+
+    public String getCurrency() {
+        return Currency;
+    }
+
+    public void setCurrency(String currency) {
+        Currency = currency;
+    }
+
+    public boolean isSuccess() {
+        return IsSuccess;
+    }
+
+    public void setSuccess(boolean success) {
+        IsSuccess = success;
+    }
+
+    public boolean isCancel() {
+        return IsCancel;
+    }
+
+    public void setCancel(boolean cancel) {
+        IsCancel = cancel;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "Id=" + Id +
+                ", Amount=" + Amount +
+                ", Currency='" + Currency + '\'' +
+                ", IsSuccess=" + IsSuccess +
+                ", IsCancel=" + IsCancel +
+                '}';
+    }
 }
--- src/main/java/victor/programMain/Program.java
+++ src/main/java/victor/programMain/Program.java
@@ -1,2 +1,63 @@
-package victor.programMain;public class Program {
+package victor.programMain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+
+
+        List<Payment> payments = initPayments();
+
+        //TODO:Implement printPayments(payments) and the code where it is called (line 13, 23)
+
+
+        //TODO:For Payment with ID=1 -> Set IsSuccess = true
+
+        //TODO:For Payment with ID=2 -> Set IsCancel = true
+
+
+        printPayments(payments);
+        chancePaymentIsSuccess(payments);
+        printPayments(payments);
+
+    }
+
+//    private static void changePaymentID(List<Payment> payments) {
+//        List<Payment> changePaymentId = new ArrayList<>();
+//        for (Payment payment : payments){
+//            if(payment.getId() )
+//        }
+//    }
+
+    private static void chancePaymentIsSuccess(List<Payment> payments) {
+
+        for(Payment payment : payments){
+            if(payment.getId()==1){
+                payment.setSuccess(true);
+
+            }
+        }
+
+    }
+
+    //print all payments
+    private static void printPayments(List<Payment> payments) {
+
+        System.out.println("----------------------");
+        for (Payment payment : payments){
+            System.out.println(payment.toString());
+        }
+    }
+
+
+    //leave this method;
+    private static List<Payment> initPayments(){
+        List<Payment> payments =  new ArrayList<>();
+        payments.add(new Payment(1,100,"EUR",false,false));
+        payments.add(new Payment(2,150,"EUR", false, false));
+        payments.add(new Payment(3,500,"RON",false  ,false));
+
+        return payments;
+    }
 }
