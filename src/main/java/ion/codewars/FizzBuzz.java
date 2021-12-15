91e44ff6fc461610cea108a2d0104188b7db0429
Ion <vaniniabivol@gmail.com>
2021-12-15 11:03:11 +0200

    FizzBuzz

---
+++ src/main/java/ion/codewars/FizzBuzz.java
@@ -0,0 +1,34 @@
+package ion.codewars;
+import java.util.stream.IntStream;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        arrayFizzBuzzer(50);
+
+    }
+
+    public static int[] arrayFizzBuzzer(int count) {
+
+        int[] arrayNumbers = IntStream.range(1, count + 1).toArray();
+
+        for (int i = 1; i <= arrayNumbers.length; i++) {
+            System.out.println(fizzBuzz(i));
+        }
+        return arrayNumbers;
+    }
+
+    private static String fizzBuzz(int i) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            return "FizzBuzz";
+        } else if (i % 5 == 0) {
+            return "Buzz";
+        } else if (i % 3 == 0) {
+            return "Fizz";
+        } else {
+            return String.valueOf(i);
+        }
+
+    }
+
+}
